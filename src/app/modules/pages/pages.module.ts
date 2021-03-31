2fe1d0e46c54d46021c510d068870fd63d31790c
Daniel <ubuntu@DESKTOP-9314L0B.localdomain>
2021-03-31 15:08:07 -0300

    feat: products page

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -4,13 +4,17 @@ import { ProductsPageComponent } from './products/products-page/products-page.co
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './products/product-card/product-card.component';
+import { CashFlowComponent } from './cash-flow/cash-flow-page/cash-flow.component';
+import { StoragePageComponent } from './storage/storage-page/storage-page.component';
 
 
 
 @NgModule({
   declarations: [
     ProductsPageComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    CashFlowComponent,
+    StoragePageComponent
   ],
   imports: [
     CommonModule,
